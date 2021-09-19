833e2d4c785bb0a72a3b6e2c602e28c38379238f
nebojsaj1726 <nebojsa.jacovic@gmail.com>
2021-09-19 11:02:30 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
+const App: React.FC = () => {
+  const [avgAge, setAvgAge] = useState<number>();
+  const [paused, setPaused] = useState<boolean>(false);
+
+  const getAvgAge = async () => {
+    try {
+      const apiUsers = await axios.get("https://randomuser.me/api/?results=10");
+
+      const usersAgeArray: number[] = [];
+
+      apiUsers.data.results.map((user: { dob: { age: number } }) =>
+        usersAgeArray.push(user.dob.age)
+      );
+
+      const sum = usersAgeArray.reduce((a: number, b: number) => a + b, 0);
+      const averageAge = sum / usersAgeArray.length;
+
+      setAvgAge(averageAge);
+    } catch (err: any) {
+      console.log(err.message);
+    }
+  };
+
+  useEffect(() => {
+    getAvgAge();
+  }, []);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (paused) {
+        clearInterval(interval);
+        return;
+      }
+      getAvgAge();
+    }, 10000);
+
+    return () => clearInterval(interval);
+  }, [paused]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Prosecna starost korisnika: {avgAge}</h3>
+      <button onClick={() => setPaused(!paused)}>
+        {paused ? "Start" : "Stop"}
+      </button>
     </div>
   );
-}
+};
 
 export default App;
